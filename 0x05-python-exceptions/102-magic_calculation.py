1ae2661b0ec6e00b5f6ad570e7ec4d95035feb6c
LeeHarvard <ade.adeyeyeadeleye@gmail.com>
2023-01-24 03:57:04 -0800

    task 102

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def magic_calculation(a, b):
     result = 0
 
@@ -8,10 +7,10 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception('Too far')
+            else:
+                result += a ** b / i
+            except:
+                result = b + a
+                break
 
-            result += a ** b / i
-        except:
-            result = b + a
-            break
-
-    return result
+            return result
